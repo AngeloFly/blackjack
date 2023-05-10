9f46d133f091c05b347d537c2950da98b2090175
hoang <48239803+hoangwork@users.noreply.github.com>
2023-05-10 10:00:09 +0800

    Update tronaddr.py

--- tronaddr.py
+++ tronaddr.py
@@ -1,93 +1,60 @@
 import base58
-import os
 import binascii
 import ecdsa
 import hashlib
-import time
-import numba as nb
+import cupy as cp
 from numba import cuda
-import numpy as np
-import logging
-from concurrent.futures import ThreadPoolExecutor
 
-logging.basicConfig(filename='tron.log', level=logging.INFO, format='%(asctime)s %(message)s')
+@cuda.jit
+def generate_tron_address_kernel(h_tron_address, h_private_key):
+    # 获取线程索引
+    thread_idx = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
 
-def generate_tron_address_private_key():
     # 生成随机私钥
-    return os.urandom(32)
+    private_key = cp.random.bytes(32)
 
-@nb.jit
-def sha256(data):
-    # Python函数，计算sha256哈希值
-    hash = hashlib.sha256(data).digest()
-    return hash
-
-@nb.jit
-def ripemd160(data):
-    # Python函数，计算ripemd160哈希值
-    hash = hashlib.new("ripemd160")
-    hash.update(data)
-    return hash.digest()
-
-@nb.jit
-def prefix(data, prefix):
-    # Python函数，添加字节前缀
-    return prefix + data
-
-@nb.jit
-def encode_base58(data):
-    # Python函数，base58编码
-    return base58.b58encode(data)
-
-@nb.jit
-def get_public_key_bytes(private_key):
-    # Python函数，获取公钥
+    # 获取公钥
     signing_key = ecdsa.SigningKey.from_string(private_key, curve=ecdsa.SECP256k1)
     verifying_key = signing_key.get_verifying_key()
-    return b"\x04" + verifying_key.to_string()
 
-@nb.jit
-def generate_tron_address_kernel(tron_address, private_key):
-    # Python函数，生成Tron地址
-    for i in range(tron_address.shape[0]):
-        # 生成随机私钥
-        private_key[i] = generate_tron_address_private_key()
+    # 添加字节前缀并哈希
+    public_key_bytes = (b"\x04" + verifying_key.to_string())
+    sha256_hash = hashlib.sha256(public_key_bytes).digest()
 
-        # 获取公钥
-        public_key_bytes = get_public_key_bytes(private_key[i])
+    # 计算ripemd160哈希
+    ripemd160_hasher = hashlib.new("ripemd160")
+    ripemd160_hasher.update(sha256_hash)
+    ripemd160_hash = ripemd160_hasher.digest()
 
-        # 添加字节前缀并哈希
-        sha256_hash = sha256(public_key_bytes)
+    # 添加字节前缀
+    prefix_ripemd160_hash = (b"\x41" + ripemd160_hash)
 
-        # 计算ripemd160哈希
-        ripemd160_hash = ripemd160(sha256_hash)
+    # 计算两次sha256哈希
+    sha256_hasher = hashlib.sha256(prefix_ripemd160_hash)
+    sha256_hash = sha256_hasher.digest()
 
-        # 添加字节前缀
-        prefix_ripemd160_hash = prefix(ripemd160_hash, b"\x41")
+    sha256_hasher2 = hashlib.sha256(sha256_hash)
+    sha256_hash2 = sha256_hasher2.digest()
 
-        # 计算两次sha256哈希
-        sha256_hash = sha256(prefix_ripemd160_hash)
-        sha256_hash2 = sha256(sha256_hash)
+    # 添加校验和并base58编码
+    address = prefix_ripemd160_hash + sha256_hash2[:4]
+    tron_address = base58.b58encode(address)
 
-        # 添加校验和并base58编码
-        address = prefix_ripemd160_hash + sha256_hash2[:4]
-        tron_address[i] = encode_base58(address)
+    # 将结果存储在cupy数组中
+    h_tron_address[thread_idx] = tron_address.decode('utf-8')
+    h_private_key[thread_idx] = binascii.hexlify(private_key).decode('utf-8')
 
-    return tron_address, private_key
+# 创建cupy数组来存储结果
+threads_per_block = 1024
+blocks_per_grid = 30
+h_tron_address = cp.empty(threads_per_block * blocks_per_grid, dtype=cp.dtype('U34'))
+h_private_key = cp.empty(threads_per_block * blocks_per_grid, dtype=cp.dtype('S32'))
 
-def generate_tron_address():
-    # 使用Numba和CUDA进行GPU加速的Tron地址生成函数
-    threads_per_block = 1024
-    blocks_per_grid = 30
-    h_tron_address = np.empty(threads_per_block * blocks_per_grid, dtype=np.dtype('U34'))
-    h_private_key = np.empty(threads_per_block * blocks_per_grid, dtype=np.dtype('S32'))
-    generate_tron_address_kernel(h_tron_address, h_private_key)
+# 在GPU上运行生成Tron地址的函数
+generate_tron_address_kernel[blocks_per_grid, threads_per_block](h_tron_address, h_private_key)
 
-    # 返回第一个非空地址和私钥
-    for i in range(len(h_tron_address)):
-        if h_tron_address[i] != '':
-            return h_tron_address[i], binascii.hexlify(h_private_key[i]).decode('utf-8')
-
-    return None
-
-print(len(generate_tron_address()[0]))
+# 找到第一个非空地址和私钥
+for i in range(len(h_tron_address)):
+    if h_tron_address[i] != '':
+        print(len(h_tron_address[i]))
+        break
