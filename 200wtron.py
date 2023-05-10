059c383640f609fb2ae300fca9a9998bca5c86c0
hoang <48239803+hoangwork@users.noreply.github.com>
2023-05-10 15:19:22 +0800

    Update 200wtron.py

--- 200wtron.py
+++ 200wtron.py
@@ -9,13 +9,13 @@ def my_kernel(io_array):
         io_array[pos] *= 2  # 对数组中的每个元素乘以2
 
 # 创建一个输入数组
-data = np.ones(256)
+data = np.ones(1000000)
 
 # 将输入数组从主机内存复制到设备内存
 data_device = cuda.to_device(data)
 
 # 定义块和网格大小
-threads_per_block = 64
+threads_per_block = 1024
 blocks_per_grid = (data.size + (threads_per_block - 1)) // threads_per_block
 
 # 调用内核函数
@@ -26,3 +26,12 @@ data_result = data_device.copy_to_host()
 
 # 打印结果
 print(data_result)
+
+# 查看GPU占用率
+device = cuda.get_current_device()
+print(f"GPU占用率: {cuda.profile_start()}")
+print(f"GPU占用率: {device.compute_capability}")
+print(f"GPU占用率: {device.max_threads_per_block}")
+print(f"GPU占用率: {device.max_block_dim_x}")
+print(f"GPU占用率: {device.max_grid_dim_x}")
+print(f"GPU占用率: {cuda.profile_stop()}")
